5464cdb7d8e58d1eeef95bd7e15b2cc4c3c248a9
Salim Hussaini <salimabdu008@gmail.com>
2021-05-03 05:38:21 -1100

    test12

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,9 +2,9 @@ require('dotenv').config()
 
 export default {
     // Target: https://go.nuxtjs.dev/config-target
-    target: 'server',
-    // mode: 'universal',
-    ssr: false,
+    target: 'static',
+    mode: 'universal',
+    // ssr: false,
     // Global page headers: https://go.nuxtjs.dev/config-head
     head: {
         title: 'Book Club',
